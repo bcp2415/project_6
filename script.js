f82727a02200586c1c7ad3c936e15d70ed3030a5
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-09-05 20:16:51 -0500

    stopping for evening; next task: figure out how to read the background color from the DOM, possibly using document.bgColor...

---
+++ script.js
@@ -0,0 +1,8 @@
+let colorBtn = document.querySelector("#changeColor");
+let colorNow =
+  // add event listener to button
+  colorBtn.addEventListener("click", function () {
+    // read and store current background color
+    // generate random new color
+    // call fcn to change background color in DOM
+  });
