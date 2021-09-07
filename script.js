ad353b26d67af600334f6189fa761824b80cbfec
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-09-07 06:34:26 -0500

    trying to add line to read current background color...have correct call but parameters not yet working

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let colorBtn = document.querySelector("#changeColor");
-let colorNow =
-  // add event listener to button
-  colorBtn.addEventListener("click", function () {
-    // read and store current background color
-    // generate random new color
-    // call fcn to change background color in DOM
-  });
+let colorNow = window
+  .getComputedStyle(html)
+  .getPropertyValue("background-color");
+console.log(colorNow);
+
+// add event listener to button
+colorBtn.addEventListener("click", function () {
+  // read and store current background color
+  // generate random new color
+  // call fcn to change background color in DOM
+});
