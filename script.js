2822b29280a2ffb17547a50a388d377328d20d53
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-09-07 19:45:04 -0500

    refactors so global variables are declared with const, and computation of current background-color happens in eventListener

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let colorBtn = document.querySelector("#changeColor");
-let screen = document.querySelector("#thisIsTheBody");
-let colorNow = window.getComputedStyle(screen);
-console.log(colorNow.getPropertyValue("background-color"));
+const colorBtn = document.querySelector("#changeColor");
+const body = document.querySelector("#thisIsTheBody");
 
 // add event listener to button
 colorBtn.addEventListener("click", function () {
   // read and store current background color
+
+  let color = window.getComputedStyle(body);
+  let colorNow = color.getPropertyValue("background-color");
+  console.log(colorNow);
+
   // generate random new color
   // call fcn to change background color in DOM
 });
