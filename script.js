549f49f148ec1cb0a420243087884ccc9b23377d
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-09-07 20:04:43 -0500

    finishes this small project; clicking button changes background color for whole window, unless new color is same as old color; in this case it calls for another new color

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const body = document.querySelector("#thisIsTheBody");
 
 // add event listener to button
 colorBtn.addEventListener("click", function () {
+  let newColor;
   // read and store current background color
 
   let color = window.getComputedStyle(body);
   let colorNow = color.getPropertyValue("background-color");
-  console.log(colorNow);
 
   // generate random new color
+  newColor = genNewColor();
+
+  // check that new color is different from current color:
+  if (newColor === colorNow) {
+    newColor = genNewColor();
+  }
   // call fcn to change background color in DOM
+  console.log(newColor);
+  body.style.backgroundColor = newColor;
 });
+
+function genNewColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  newColor = String(`rgb(${red}, ${green}, ${blue})`);
+  return newColor;
+}
