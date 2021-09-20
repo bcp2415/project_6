5921910dbf5ab6606af50073c159c18c4f58c124
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-09-19 21:03:09 -0500

    adds functions to convert hex color values to rgb values

--- script.js
+++ script.js
@@ -98,9 +98,54 @@ function rgbFlip() {
   return String(`rgb(${red}, ${green}, ${blue})`);
 }
 
-function hexToRgb(hexColor) {
+function colorConvert(hexColor) {
   // convert hex value received as a string to an array w/ 3 members
+  let hexArray = Array.from(hexColor);
+
+  let red = [hexArray[1], hexArray[2]];
+  let green = [hexArray[3], hexArray[4]];
+  let blue = [hexArray[5], hexArray[6]];
+
   // convert each 2-digit value to a decimal number from 0-255
+  let redRgb = hexToRgb(red);
+  console.log(redRgb);
+  let greenRgb = hexToRgb(green);
+  console.log(greenRgb);
+  let blueRgb = hexToRgb(blue);
+  console.log(blueRgb);
   // assemble string to return:  'rgb(r, g, b)'
-  return redGreenBlueValue;
+  let finalRgb = `rgb(${redRgb}, ${greenRgb}, ${blueRgb})`;
+  // return redGreenBlueValue;
+  return finalRgb;
 }
+
+function hexToRgb(hexArray) {
+  let decimalValue = 0;
+  let ones = convertHexToDecimal(hexArray[1]);
+
+  let sixteens = convertHexToDecimal(hexArray[0]) * 16;
+  decimalValue = ones + sixteens;
+  return decimalValue;
+}
+
+function convertHexToDecimal(digit) {
+  let decimal = 0;
+  if (digit === "a") {
+    decimal = 10;
+  } else if (digit === "b") {
+    decimal = 11;
+  } else if (digit === "c") {
+    decimal = 12;
+  } else if (digit === "d") {
+    decimal = 13;
+  } else if (digit === "e") {
+    decimal = 14;
+  } else if (digit === "f") {
+    decimal = 15;
+  } else if (Number(digit) <= 9) {
+    decimal += Number(digit);
+  }
+  return decimal;
+}
+
+console.log(colorConvert("#ffffff"));
